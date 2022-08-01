894aa50bb6ed662d5a1a340c1818b231306f103d
Najmul H. Bappy <http.bappy@gmail.com>
2022-08-01 23:21:19 +0600

    updated

--- src/com/bappy4u/Main.java
+++ src/com/bappy4u/Main.java
@@ -1,13 +1,47 @@
 package com.bappy4u;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        User bappy = new User("Bappy");
+        System.out.println("Welcome to the tweeter!");
 
-        bappy.createTweet("Hi, This is my first tweet.");
+        System.out.println("you can use this tweeter just only put your name.");
+        System.out.print("If you want to use tweet type 'yes':");
 
-        bappy.tweets();
+        Scanner sc = new Scanner(System.in);
+        String yes = sc.nextLine();
+        yes = yes.toLowerCase();
+        while (!yes.equals("yes")){
+            System.out.println("Type error! Please Enter 'yes' to continue.");
+            yes = sc.nextLine();
+            yes = yes.toLowerCase();
+        }
+        System.out.print("Please enter your name:");
+        User user = new User(sc.nextLine());
+
+        System.out.println("Hi "+user.name+ ", Thanks for the joining!");
+
+        System.out.print("If you want to post a tweet enter 1 or if you want to see your tweet enter 'tweets':");
+        String userIn = sc.nextLine();
+        userIn = userIn.toLowerCase();
+
+        while (!(userIn.equals("1") || userIn.equals("tweets"))){
+            System.out.println("Type error! Please Enter '1' or 'tweets' to continue.");
+            userIn = sc.nextLine();
+            userIn= userIn.toLowerCase();
+        }
+        if (userIn.equals("1")){
+            user.createTweet(sc.nextLine());
+        }
+        else{
+            System.out.println(user.name + ", this is all your tweets:");
+            user.tweets();
+        }
+
+
+
     }
 }
