7a06bb8cd01f3e008ff3332e57757d61459942ef
Najmul H. Bappy <http.bappy@gmail.com>
2022-08-01 22:16:08 +0600

    updated

--- src/com/bappy4u/Tweet.java
+++ src/com/bappy4u/Tweet.java
@@ -2,9 +2,19 @@ package com.bappy4u;
 
 public class Tweet {
     private String date;
+    private String tweet;
     public Tweet()
     {
         System.out.println("Tweet is created.");
+
+    }
+    public void setTweet(String tweet){
+        if (tweet.length()>120){
+            this.tweet = tweet;
+        }
+        else{
+            System.out.println("you can not tweet a text over 120 characters");
+        }
 
     }
 }
